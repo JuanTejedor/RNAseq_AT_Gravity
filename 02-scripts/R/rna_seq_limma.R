8b496af5ff3c3693d50968df1b4e6d1060329ce1
JuanTejedor <juan.a.t.s2001@hotmail.es>
2024-01-24 19:35:33 +0100

    Cambios script R de gravedad a humedad

--- 02-scripts/R/rna_seq_limma.R
+++ 02-scripts/R/rna_seq_limma.R
@@ -46,7 +46,7 @@ dim(gene.expression)
 gene.names <- rownames(gene.expression)
 
 ## Nombramos las columnas con los nombres de nuestras muestras. 
-colnames(gene.expression) <- c("control1","control2","control3","moon1","moon2","moon3")
+colnames(gene.expression) <- c("h_alta1","h_alta2","h_alta3","h_alta4","h_baja1","h_baja2","h_baja3","h_baja4")
 
 ## Por motivos técnicos sumamos 1 a todos los niveles de expresión. 
 ## El problema viene provocado por x < 1 --> log2(x) < 0
@@ -58,7 +58,7 @@ write.table(x = gene.expression.1,file = "04-results/gene_expression_unprocessed
             sep = "\t")
 
 ## Representación de la distribución global de la expresión génica
-color=c(rep("grey",3), rep("purple",3))
+color=c(rep("cyan4",4), rep("grey",4))
 boxplot(gene.expression, outline=F,col=color,ylab="Gene Expression (FPKM)",
         cex.lab=1.5)
 
@@ -74,16 +74,16 @@ boxplot(log2(gene.expression.1), outline=F,col=color,
 library(NormalyzerDE)
 
 design <- data.frame(sample=colnames(gene.expression),
-                     group=c(rep("control",3),rep("moon",3)))
+                     group=c(rep("h_alta",4),rep("h_baja",4)))
 
 write.table(x = design,file = "01-documentation/normalyzer_design.tsv",quote = F,row.names = F,
             sep = "\t")
 
-normalyzer(jobName = "AT_Gravity_norm",designPath = "01-documentation/normalyzer_design.tsv",
+normalyzer(jobName = "AT_Humedad_norm",designPath = "01-documentation/normalyzer_design.tsv",
            dataPath = "04-results/gene_expression_unprocessed.tsv",outputDir = "04-results/"[])
 # Tras ver los resultados, parece ser que CycLoess es el mejor método de normalización pues consigue una baja variabilidad intragrupo, un alto coeficiente de correlación intragrupo, agrupa bien las muestras y, además, no elimina diferencias significativas entre condiciones.
 
-normalized.gene.expression <- read.table(file="04-results/AT_Gravity_norm/CycLoess-normalized.txt", header=T)
+normalized.gene.expression <- read.table(file="04-results/AT_Humedad_norm/Quantile-normalized.txt", header=T)
 head(normalized.gene.expression)
 rownames(normalized.gene.expression) <- gene.names
  
@@ -95,13 +95,13 @@ boxplot(normalized.gene.expression, outline=F,col=color,
 ## Previsualizamos la similitud entre las réplicas
 
 
-scatter_replicates <- function(obj,comparation_matrix, color=c("grey","purple")) {
+scatter_replicates <- function(obj,comparation_matrix, color=c("cyan4","grey")) {
   par(mfrow=c(ncol(comparation_matrix),c(nrow(comparation_matrix)/ncol(comparation_matrix))))
   for (i in 1:nrow(comparation_matrix)) {
     a=comparation_matrix[i,1]
     b=comparation_matrix[i,2]
     index_color=1
-    if (i>3) {
+    if (i>c(nrow(comparation_matrix)/ncol(comparation_matrix))) {
       index_color=2
     }
     plot(x = obj[,a],
@@ -118,7 +118,7 @@ scatter_replicates <- function(obj,comparation_matrix, color=c("grey","purple"))
   par(mfrow=c(1,1))
 }
 
-comparaciones = matrix(c(1,2,1,3,2,3,4,5,4,6,5,6), nrow = 6, ncol = 2, byrow = T)
+comparaciones = matrix(c(1,2,1,3,1,4,2,3,2,4,3,4,5,6,5,7,5,8,6,7,6,8,7,8), nrow = 12, ncol = 2, byrow = T)
 scatter_replicates(normalized.gene.expression,comparaciones)
 
 
@@ -126,6 +126,7 @@ scatter_replicates(normalized.gene.expression,comparaciones)
 ## jerárquico para continuar con la exploración de los datos.
 library(FactoMineR)
 library(factoextra)
+library(cluster)
 
 ## Los dos paquetes anteriores son geniales para análisis estadísticos en
 ## datos de secuencaición de nueva generación. Tienen una documentación muy 
@@ -144,7 +145,7 @@ head(pca.gene.expression)
 ## PCA
 res.pca <- PCA(pca.gene.expression, graph = FALSE,scale.unit = TRUE,quali.sup = 1 )
 fviz_screeplot(res.pca)
-fviz_pca_ind(res.pca, col.ind = c(rep("Control",3),rep("Moon",3)), 
+fviz_pca_ind(res.pca, col.ind = c(rep("Humedad Alta",4),rep("Humedad Baja",4)), 
              pointsize=2, pointshape=21,fill="black",
              repel = TRUE, 
              addEllipses = TRUE,ellipse.type = "confidence",
@@ -174,24 +175,24 @@ fviz_dend(res.agnes,k=2,
 
 
 ## Cluster partición
-res.agnes <- cluster::pam(pca.gene.expression, k=2)
 fviz_nbclust(x=pca.gene.expression[,-1], method = "gap_stat",FUNcluster = cluster::pam, k=5)
 fviz_nbclust(x=pca.gene.expression[,-1], method = "wss",FUNcluster = cluster::pam, k=5)
-fviz_nbclust(x=pca.gene.expression[,-1], method = "gap_stat",FUNcluster = cluster::pam, k=5)
+fviz_nbclust(x=pca.gene.expression[,-1], method = "silhouette",FUNcluster = cluster::pam, k=5)
+res.pam <- cluster::pam(pca.gene.expression, k=2)
 
 
 ## Calculamos la matrix de expresión media. 
-control <- apply(normalized.gene.expression[1:3], MARGIN = 1,FUN = mean)
-moon <- apply(normalized.gene.expression[4:6], MARGIN = 1,FUN = mean)
+h_alta <- apply(normalized.gene.expression[1:4], MARGIN = 1,FUN = mean)
+h_baja <- apply(normalized.gene.expression[5:8], MARGIN = 1,FUN = mean)
 
-mean.expression <- matrix(c(control,moon),ncol=2)
-colnames(mean.expression) <- c("control","moon")
+mean.expression <- matrix(c(h_alta,h_baja),ncol=2)
+colnames(mean.expression) <- c("h_alta","h_baja")
 rownames(mean.expression) <- rownames(normalized.gene.expression)
 head(mean.expression)
 
 ## Previsualizamos el efecto de la mutación en un scatterplot.
-plot(control,moon,pch=19,cex=0.7,xlab="Control",
-     ylab=substitute(italic("Moon")),
+plot(h_baja,h_alta,pch=19,cex=0.7,xlab="Humedad Baja",
+     ylab=substitute(italic("Humedad Alta")),
      cex.lab=1.25,
      col="grey")
 
@@ -203,13 +204,15 @@ library(limma)
 
 ## Especificamos el diseño experimental
 
-experimental.design <- model.matrix(~ -1+factor(c(1,1,1,2,2,2)))
-colnames(experimental.design) <- c("control","moon")
+experimental.design <- model.matrix(~ -1+factor(c(1,1,1,1,2,2,2,2)))
+colnames(experimental.design) <- c("h_baja","h_alta")
 
 ##A continuación, ajustamos la estimación de los niveles de expresión de cada
 ##gen a un modelo lineal teniendo en cuenta el diseño experimental. Este paso
 ##fundamentalmente se calcula la media de las réplicas en cada condición.
 
+# Cambio el orden de las columnas para poner la humedad baja como referencia. 
+normalized.gene.expression <- normalized.gene.expression[c(5:8,1:4)]
 linear.fit <- lmFit(normalized.gene.expression, experimental.design)
 
 ##Para especificar los constrastes a realizar utilizamos la función
@@ -218,7 +221,7 @@ linear.fit <- lmFit(normalized.gene.expression, experimental.design)
 ##correspondientes separadas por un guión -. También recibe el argumento 
 ##levels, un vector con el nombre de las condiciones:
 
-contrast.matrix <- makeContrasts(moon-control,levels=c("control","moon"))
+contrast.matrix <- makeContrasts(h_alta-h_baja,levels=c("h_baja","h_alta"))
 
 ##Calculamos el fold-change y los p-valores correspondientes para cada gen en
 ##cada uno de los constrastes especificados utilizando las funciones *constrasts.fit* 
@@ -229,12 +232,12 @@ contrast.results <- eBayes(contrast.linear.fit)
 
 nrow(normalized.gene.expression)
 
-control.moon <- topTable(contrast.results, number=7507,coef=1,sort.by="logFC")
-head(control.moon)
+control.h_alta <- topTable(contrast.results, number=7507,coef=1,sort.by="logFC")
+head(control.h_alta)
 
-log.fold.change <- control.moon$logFC
-q.value <- control.moon$adj.P.Val
-genes.ids <- rownames(control.moon)
+log.fold.change <- control.h_alta$logFC
+q.value <- control.h_alta$adj.P.Val
+genes.ids <- rownames(control.h_alta)
 names(log.fold.change) <- genes.ids
 names(q.value) <- genes.ids
 
@@ -258,6 +261,7 @@ points(x = log.fold.change[repressed.genes],
 
 ## Enriquecimiento funcional. 
 library(clusterProfiler)
+library(enrichplot)
 library(org.At.tair.db)
 
 activated.atha.enrich.go <- enrichGO(gene          = activated.genes,
@@ -268,10 +272,10 @@ activated.atha.enrich.go <- enrichGO(gene          = activated.genes,
                                      readable      = FALSE,
                                      keyType = "TAIR")
 
-barplot(activated.atha.enrich.go,showCategory = 20)
-dotplot(activated.atha.enrich.go,showCategory = 20)
-emapplot(activated.atha.enrich.go,showCategory = 20)
-cnetplot(activated.atha.enrich.go,showCategory = 20)
+barplot(activated.atha.enrich.go,showCategory = 20, font.size=8)
+dotplot(activated.atha.enrich.go,showCategory = 20, font.size=8)
+emapplot(pairwise_termsim(activated.atha.enrich.go),showCategory = 20, repel=T, cex_label_category=0.6)
+cnetplot(activated.atha.enrich.go,showCategory = 20,repel=T, cex_label_category=0.6,cex_label_gene=0.4, max.overlaps=5)
 
 
 repressed.atha.enrich.go <- enrichGO(gene          = repressed.genes,
@@ -281,11 +285,10 @@ repressed.atha.enrich.go <- enrichGO(gene          = repressed.genes,
                                      pvalueCutoff  = 0.05,
                                      readable      = FALSE,
                                      keyType = "TAIR")
-
-barplot(repressed.atha.enrich.go,showCategory = 20)
-dotplot(repressed.atha.enrich.go,showCategory = 20)
-emapplot(repressed.atha.enrich.go,showCategory = 20)
-cnetplot(repressed.atha.enrich.go,showCategory = 20)
+barplot(activated.atha.enrich.go,showCategory = 20, font.size=8)
+dotplot(activated.atha.enrich.go,showCategory = 20, font.size=8, group=T)
+emapplot(pairwise_termsim(activated.atha.enrich.go),showCategory = 20, repel=T, cex_label_category=0.6)
+cnetplot(activated.atha.enrich.go,showCategory = 20,repel=T, cex_label_category=0.6,cex_label_gene=0.4, max.overlaps=5)
 
 
 activated.atha.enrich.kegg <- enrichKEGG(gene  = activated.genes,
@@ -306,25 +309,29 @@ head(df.repressed.atha.enrich.kegg)
 
 ## Podemos visualizar las rutas KEGG usando el paquete pathveiw
 library(pathview)
-pathview(gene.data = sort(log.fold.change,decreasing = TRUE),
-         pathway.id = "ath00941",
-         species = "ath",
-         limit = list(gene=max(abs(log.fold.change)), cpd=1),gene.idtype = "TAIR")
+rutas=c("ath01100","ath01110","ath00030", "ath00190","ath00510", "ath00513", "ath00905","ath04075", "ath04626", "ath00010", "ath00513", "ath00514")
+
+for (i in rutas) {
+  pathview(gene.data = sort(log.fold.change,decreasing = TRUE),
+           pathway.id = i,
+           species = "ath",
+           limit = list(gene=max(abs(log.fold.change)), cpd=1),gene.idtype = "TAIR",kegg.dir="04-results/pathview/")
+}
 
 
-
+coolmap(normalized.gene.expression)
 
 ## Código para desarrollar una función gráfico de barras
 
 gene <- activated.genes[1]
 
 expression.matrix <- 2^normalized.gene.expression - 1
-cond.names <- c("col0","abc")
+cond.names <- c("Humedad Baja","Humedad Alta")
 
 gene.expression.barplot <- function(gene, expression.matrix,cond.names)
 {
-  expr.1 <- unlist(c(expression.matrix[gene, 1:2]))
-  expr.2 <- unlist(c(expression.matrix[gene, 3:4]))
+  expr.1 <- unlist(c(expression.matrix[gene, 1:4]))
+  expr.2 <- unlist(c(expression.matrix[gene, 5:8]))
   
   mean.1 <- mean(expr.1)
   mean.2 <- mean(expr.2)
